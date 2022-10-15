16c265c7addbabbce0a8cbbeed45fe1baa8e8a90
Jordan <jcstoffer2@gmail.com>
2022-10-15 12:51:57 -0700

    Add groovy for spock, stub out Rectangle model and spec

---
+++ src/main/java/org/example/model/Rectangle.java
@@ -0,0 +1,5 @@
+package org.example.model;
+
+public class Rectangle {
+    // TODO: add properties and methods
+}
---
+++ src/test/groovy/org/example/model/RectangleSpecification.groovy
@@ -0,0 +1,10 @@
+package org.example.model
+
+import spock.lang.Specification
+
+class RectangleSpecification extends Specification {
+    def "a simple assertion test"() {
+        expect:
+        1 == 1
+    }
+}
