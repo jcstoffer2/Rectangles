1b2bcab8ef0aef78135a91ec5a52ff60c1c25ef5
Jordan <jcstoffer2@gmail.com>
2022-10-15 13:49:23 -0700

    Implement intersect method and test positive and negative scenarios

--- src/main/java/org/example/model/Rectangle.java
+++ src/main/java/org/example/model/Rectangle.java
@@ -2,15 +2,42 @@ package org.example.model;
 
 /**
  * A class to represent a rectangle object
+ *
  * @Author Jordan Stoffer
  */
 public class Rectangle {
-    // TODO: add properties and methods
+    private int x; // top left x coordinate of the rectangle
+    private int y; // top left y coordinate of the rectangle
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
     /**
-     * Returns true if the parameter rectangle intersects
-     * with this Rectangle instance.
+     * Returns true if the parameter anotherRectangle
+     * intersects with this Rectangle.
      */
-    private boolean intersects(Rectangle rectangle) {
-        return true;
+    boolean intersects(Rectangle anotherRectangle) {
+        boolean intersects = false;
+        int x = anotherRectangle.x;
+        int y = anotherRectangle.y;
+        int width = anotherRectangle.width;
+        int height = anotherRectangle.height;
+
+        if (
+                x + width > this.x && // x coordinate is inside this rectangle
+                y + height > this.y && // y coordinate is inside this rectangle
+                x < this.x + this.width &&
+                y < this.y + this.height
+        ) {
+            intersects = true;
+        }
+
+        return intersects;
     }
 }
--- src/test/groovy/org/example/model/RectangleSpecification.groovy
+++ src/test/groovy/org/example/model/RectangleSpecification.groovy
@@ -3,17 +3,18 @@ package org.example.model
 import spock.lang.Specification
 
 class RectangleSpecification extends Specification {
-    def "a simple assertion test"() {
-        expect:
-        1 == 1
-    }
 
     def "tests whether a rectangle intersects"() {
         given: "a rectangle"
-        Rectangle aRectangle = new Rectangle();
-        when: "another rectangle intersects"
-        Rectangle intersectingRectangle = new Rectangle();
-        then:
-        aRectangle.intersects(intersectingRectangle) == true;
+        Rectangle aRectangle = new Rectangle(10, 20, 40, 20)
+        when: "the values set fall within the original rectangle"
+        Rectangle intersectingRectangle = new Rectangle(10, 10, 20, 40)
+        then: "the rectangles intersect"
+        aRectangle.intersects(intersectingRectangle)
+
+        when: "the values set fall outside of the original rectangle"
+        Rectangle nonIntersectingRectangle = new Rectangle(50, 50, 10, 10)
+        then: "the rectangles do not intersect"
+        !aRectangle.intersects(nonIntersectingRectangle)
     }
 }
